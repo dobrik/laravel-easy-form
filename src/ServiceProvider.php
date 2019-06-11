b09f40b5acf60538ca4e5fe640ab481e5b9685d5
Dobrik <dobrik1990@gmail.com>
2019-06-12 00:27:41 +0300

    service provider configuration fixed

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -9,28 +9,27 @@ class ServiceProvider extends \Illuminate\Support\ServiceProvider
 {
     public function register()
     {
-        $this->mergeConfigFrom($this->getAliasesConfigPath(), 'easy_form/aliases');
-        $this->mergeConfigFrom($this->getFormsConfigPath(), 'easy_form/forms');
+        $this->mergeConfigFrom($this->getAliasesConfigPath(), 'easy_form.aliases');
+        $this->mergeConfigFrom($this->getFormsConfigPath(), 'easy_form.forms');
 
         $this->app->singleton(Creator::class, function () {
-            $creator = new Creator(new Factory());
+            $configResolver = $this->app->make('config');
+            $creator = new Creator(new Factory($configResolver->get('easy_form.aliases')));
             return $creator->setRequest($this->app->make(\Illuminate\Http\Request::class))
-                ->registerFormConfig($this->app->make('config')->get('easy_form.forms'));
+                ->registerFormConfig($configResolver->get('easy_form.forms'));
         }
         );
     }
 
     public function boot()
     {
-        $this->loadViewsFrom(__DIR__ . '/resources/views', 'easy_form');
+        $this->loadViewsFrom($this->getViewsPath(), 'easy_form');
 
-        $this->publishes([
-            __DIR__ . '/resources/views' => resource_path('views/vendor/easy_form'),
-        ]);
 
         $this->publishes([
             $this->getAliasesConfigPath() => config_path('easy_form/aliases.php'),
-            $this->getFormsConfigPath() => config_path('easy_form/forms.php')
+            $this->getFormsConfigPath() => config_path('easy_form/forms.php'),
+            $this->getViewsPath() => resource_path('views/vendor/easy_form'),
         ]);
     }
 
@@ -42,5 +41,10 @@ class ServiceProvider extends \Illuminate\Support\ServiceProvider
     private function getFormsConfigPath()
     {
         return __DIR__ . '/resources/config/forms.php';
+    }
+
+    private function getViewsPath(): string
+    {
+        return __DIR__ . '/resources/views';
     }
 }
