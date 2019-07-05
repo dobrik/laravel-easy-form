31a15e1c42b6f73fe754f4fba5c366765968a860
Dobrik <a.dobrovolsky@dinarys.com>
2019-07-05 11:16:34 +0300

    remove tabs label when used one tab

--- src/resources/views/html/tabs.blade.php
+++ src/resources/views/html/tabs.blade.php
@@ -1,18 +1,20 @@
 <h4>{{ \Illuminate\Support\Arr::pull($attributes, 'label') }}</h4>
 <div class="nav-tabs-custom">
-    <ul @forelse($attributes as $attribute => $value) {{ $attribute }}="{{ $value }}" @empty @endforelse>
+    @if(count($tabs) > 1)
+        <ul @forelse($attributes as $attribute => $value) {{ $attribute }}="{{ $value }}" @empty @endforelse>
         @forelse($tabs as $key => $tab)
-        <li class="@if($key === 0) active @endif">
-            <a data-toggle="tab" aria-expanded="true"
-               href="#{{ $tab->attributes['id'] }}">{{ $tab->attributes['title'] }}</a>
-        </li>
-        @empty
-        @endforelse
-        </ul>
-        <div class="tab-content">
-            @forelse($tabs as $key => $tab)
-                {!! $tab !!}
+            <li class="@if($key === 0) active @endif">
+                <a data-toggle="tab" aria-expanded="true"
+                   href="#{{ $tab->attributes['id'] }}">{{ $tab->attributes['title'] }}</a>
+            </li>
             @empty
             @endforelse
-        </div>
+            </ul>
+            @endif
+            <div class="tab-content">
+                @forelse($tabs as $key => $tab)
+                    {!! $tab !!}
+                @empty
+                @endforelse
+            </div>
 </div>
